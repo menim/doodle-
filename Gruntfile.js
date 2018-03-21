ba590f11217437fd67212c3054e87fe3f577640e
tol <tol@Mac-mini-tol.local>
2018-03-21 12:41:01 +0200

    lint code with google style and js lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,145 +1,141 @@
-module.exports=function(grunt){
+module.exports=function(grunt) {
   grunt.initConfig({
-    pkg:grunt.file.readJSON('package.json'),
-    concat:{
-      options:{
-        separator:';'
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
       },
-      dist:{
-        src:['js/myown.js'],
-        dest:'dist/myown.min.js'
-      }
+      dist: {
+        src: ['js/myown.js'],
+        dest: 'dist/myown.min.js',
+      },
     },
-    uncss:{
-      dist:{
-        files:[
-        { src:'index.html',dest:'dist/myown.css'}
-        ]
-      }
+    uncss: {
+      dist: {
+        files: [
+        {src: 'index.html', dest: 'dist/myown.css'},
+        ],
+      },
     },
-    uglify:{
-      my_target:{
-        files:{
-          'dist/myown.js':['js/myown.js']
-        }
-      }
+    uglify: {
+      my_target: {
+        files: {
+          'dist/myown.js': ['js/myown.js'],
+        },
+      },
     },
-    jshint:{
-      files:['Gruntfile.js','js/*.js'],
-      options:{
+    jshint: {
+      files: ['Gruntfile.js', 'js/*.js'],
+      options: {
         globals:
         {
-          jQuery:true,
-          console:true,
-          module:true,
-          document:true
-        }
-      }
-    },
-    htmlmin:{
-      dist:{
-        options:{
-          removeComments:true,
-          collapseWhitespace:true
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true,
         },
-        files:{
-          'dist/index.html':'index.html'
-        }
-      }
+      },
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          'dist/index.html': 'index.html',
+        },
+      },
     },
     htmllint: {
-      all:['index.html']
+      all: ['index.html'],
     },
-    csslint:{
-      src:['css/main.css']
+    csslint: {
+      src: ['css/main.css'],
     },
-    imageoptim:{
-      myTask:{
-        options:{
-          jpegMini:false,
-          imageAlpha:true,
-          quitAfter:true
+/*    imageoptim: {
+      myTask: {
+        options: {
+          jpegMini: false,
+          imageAlpha: true,
+          quitAfter: true,
         },
-        src:['images']
-      }
-    },
-    cssmin:{
+        src: ['images'],
+      },
+    },*/
+    cssmin: {
       target: {
-        files:[
+        files: [
           {
-            src:'css/main.css',
-            dest:'dist/main.css'
-          }
-        ]
-      }
+            src: 'css/main.css',
+            dest: 'dist/main.css',
+          },
+        ],
+      },
     },
-    concat_css:{
-      options:{
+    concat_css: {
+      options: {
 
       },
-      all:{
-        src:["css/*.css"],
-        dest:"dist/main.css"
-      }
+      all: {
+        src: ['css/*.css'],
+        dest: 'dist/main.css',
+      },
     },
     postcss: {
-    options: {
-      map: {
-          inline: false,
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')([
+            'Android 2.3',
+            'Android >= 4',
+            'Chrome >= 20',
+            'Firefox >= 3',
+            'Explorer 9',
+            'iOS >= 6',
+            'Opera >= 8',
+            'Safari >= 6',
+          ]),
+        ],
       },
-
-      processors: [
-        require('autoprefixer')([
-          "Android 2.3",
-          "Android >= 4",
-          "Chrome >= 20",
-          "Firefox >= 3",
-          "Explorer 9",
-          "iOS >= 6",
-          "Opera >= 8",
-          "Safari >= 6"
-        ])
-        /*require('cssgrace'),*/
-      ]
+      dist: {
+        src: '*.css',
+      },
     },
-    dist: {
-      src: 'css/main.css'
-    }
-  },
-  watch:{
+    watch: {
       sass: {
         files: ['sass/**/*.scss'],
-        tasks: ['sass']
+        tasks: ['sass'],
       },
     },
     sass: {
       dev: {
         files: {
-          'css/main.css' : 'sass/main.scss'
-        }
-      }
+          'css/main.css': 'sass/main.scss',
+        },
+      },
     },
-    browserSync:{
-      dev:{
-        bsFiles:{
-          src:[
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
           'css/*.css',
-          'index.html']
+          'index.html'],
         },
         options: {
           watchTask: true,
-          server: './'
-        }
-      }
+          server: './',
+        },
+      },
     },
-    stylelint:{
-      simple:{
+    stylelint: {
+      simple: {
         options: {
           configFile: './.stylelintrc.json',
-          format: 'sass'
+          format: 'sass',
         },
-        src:'sass/**/*.scss'
-      }
+        src: 'sass/**/*.scss',
+      },
     },
     responsive_images: {
       myTask: {
@@ -147,27 +143,27 @@ module.exports=function(grunt){
           sizes: [
           {
             width: 320,
-            height: 240
+            height: 240,
           },
           {
             name: 'large',
-            width: 640
+            width: 640,
           },
           {
             name: 'large',
             width: 1024,
-            suffix: "_x2",
-            quality: 60
-          }]
+            suffix: '_x2',
+            quality: 60,
+          }],
         },
         files: [{
           expand: true,
           src: ['images/**.{gif,jpg,png}'],
-          dest: 'tmp/'
-        }]
-      }
-    }
-  });   
+          dest: 'tmp/',
+        }],
+      },
+    },
+  });
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -175,7 +171,7 @@ module.exports=function(grunt){
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-imageoptim');
+/*  grunt.loadNpmTasks('grunt-imageoptim');*/
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -185,7 +181,6 @@ module.exports=function(grunt){
   grunt.loadNpmTasks('grunt-stylelint');
   grunt.loadNpmTasks('grunt-responsive-images');
 
-
   grunt.registerTask('default', ['browserSync', 'watch']);
-  grunt.registerTask('ondev', ['postcss','cssmin','htmlmin','uglify']);
+  grunt.registerTask('ondev', ['postcss', 'cssmin', 'htmlmin', 'uglify']);
 };
--- js/myown.js
+++ js/myown.js
@@ -1,43 +1,46 @@
 $(document).ready(function() {
-  
-  /*Intial config*/
+  /* Intial config */
 
-  "use strict";
-  $(".fancybox").fancybox({
+  'use strict';
+
+  $('.fancybox').fancybox({
     padding: 0,
     helpers: {
       title: null,
       overlay: {
-        locked: false
-      }
-    }
+        locked: false,
+      },
+    },
   });
 
-  $("#doodle-slider-wrapper").slick({
+  $('#doodle-slider-wrapper').slick({
     infinite: true,
     speed: 1000,
     fade: true,
-    cssEase: "linear",
+    cssEase: 'linear',
     arrows: true,
-    mobilefirst: true
+    mobilefirst: true,
   });
 
-  document.body.className += " loaded";
-  
+  document.body.className += ' loaded';
 
-  /*Appear images and text on main page, when scroll*/
+  /* Appear images and text on main page, when scroll */
 
-  var windowVar = $(window), hideme = $(".hideme"), hidemeLen = hideme.length;
+  var windowVar = $(window);
+  var hideme = $(".hideme");
+  var hidemeLen = hideme.length;
+
   windowVar.scroll(function() {
     for (var i = 0; i < hidemeLen; i++) {
-      var self = $(hideme[i]),
-        bottomOfObject = self.offset().top + self.outerHeight(),
-        bottomOfWindow = windowVar.scrollTop() + windowVar.height();
+      var self = $(hideme[i]);
+      var bottomOfObject = self.offset().top + self.outerHeight();
+      var bottomOfWindow = windowVar.scrollTop() + windowVar.height();
+
       if (bottomOfWindow > bottomOfObject) {
-        if (self.hasClass("doodle-item__description")) {
-          self.animate({ right: 0 }, 700).animate({ opacity: 1 });
+        if (self.hasClass('doodle-item__description')) {
+          self.animate({right: 0}, 700).animate({opacity: 1});
         } else {
-          self.animate({ left: 0, opacity: 1 }, 700).animate({ opacity: 1 });
+          self.animate({left: 0, opacity: 1}, 700).animate({opacity: 1});
         }
       }
     }
@@ -48,18 +51,18 @@ $(document).ready(function() {
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
         location.hostname == this.hostname
       ) {
         var target = $(this.hash);
         target = target.length
           ? target
-          : $("[name=" + this.hash.slice(1) + "]");
+          : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
-          $("html, body").animate(
+          $('html, body').animate(
             {
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top,
             },
             1000
           );
@@ -68,36 +71,36 @@ $(document).ready(function() {
       }
     });
 
-    /* swap to gallery page when click on ribbon with "watch pages" inscription */
+/* swap to gallery page when click on ribbon with "watch pages" inscription */
 
-    var self = null,
-      container = null,
-      items = null,
-      frstChild = null,
-      secChild = null,
-      elem = null,
-      elemLen = null,
-      containerPreview = null;
+    var self = null;
+    var container = null;
+    var items = null;
+    var frstChild = null;
+    var secChild = null;
+    var elem = null;
+    var elemLen = null;
+    var containerPreview = null;
 
-    $(".doodle-item__ribbon").click(function(e) {
+    $('.doodle-item__ribbon').click(function(e) {
       e.preventDefault();
       self = $(this);
       container = self.parent();
-      items = container.find(".doodle-item__container");
+      items = container.find('.doodle-item__container');
       frstChild = items.eq(0);
       secChild = items.eq(1);
-      elem = container.find(".preview");
+      elem = container.find('.preview');
       elemLen = elem.length;
       containerPreview = elem.parent();
 
-      frstChild.toggleClass("clicked");
-      secChild.toggleClass("clicked-two");
-      self.parent().toggleClass("clicked-ribbon");
-      self.children().toggleClass("rotate");
+      frstChild.toggleClass('clicked');
+      secChild.toggleClass('clicked-two');
+      self.parent().toggleClass('clicked-ribbon');
+      self.children().toggleClass('rotate');
 
       setTimeout(function() {
         for (var j = 0; j < elemLen; j++) {
-          if ($(elem[j]).css("display") === "none") {
+          if ($(elem[j]).css('display') === 'none') {
             $(elem[j]).delay(j * 150).fadeIn(200);
           }
         }
@@ -108,69 +111,82 @@ $(document).ready(function() {
   /* Fill all svg images */
 
   setTimeout(function() {
-    var el = document.querySelectorAll(".st0"), len = el.length;
+    var el = document.querySelectorAll(".st0");
+    var len = el.length;
     for (var i = 0; i < len; i++) {
-      el[i].setAttribute("fill", "gray");
+      el[i].setAttribute('fill', 'gray');
     }
   }, 5000);
 });
 
 /* collapse-extend menu on mobile */
 
-var hamburger_btn = document.querySelector(".hamburger-btn"),
-  header = document.querySelector("header"),
-  headWrapper = document.querySelector("header .container"),
-  h = headWrapper.clientHeight,
-  additional_class = " is-active";
+var hamburger_btn = document.querySelector('.hamburger-btn');
+var header = document.querySelector("header");
+var headWrapper = document.querySelector("header .container");
+var h = headWrapper.clientHeight;
+var additionalClass = ' is-active';
 
-hamburger_btn.addEventListener("click", function() {
+hamburger_btn.addEventListener('click', function() {
   if (parseInt(header.style.height) !== h) {
-    this.className += additional_class;
-    header.style.height = h + "px";
+    this.className += additionalClass;
+    header.style.height = h + 'px';
   } else {
-    header.style.height = "130px";
-    this.className = this.className.replace(" is-active", "");
+    header.style.height = '130px';
+    this.className = this.className.replace(' is-active', '');
   }
 });
 
 /* FORM VALIDATION */
 
+/** Validate text input with name, should not be empty and must consist of letters
+   @param {string} name - value of input
+   @return {bool}
+*/
 function validateName(name) {
   var re = /^[а-яА-яіІЇїЄєґҐёЁA-Za-z]+$/;
-  if (name.value === "" || !re.test(name.value)) {
-    name.nextElementSibling.innerHTML = "Вы ввели неправильное имя";
-    name.nextElementSibling.className = "error is-active-error";
+  if (name.value === '' || !re.test(name.value)) {
+    name.nextElementSibling.innerHTML = 'Вы ввели неправильное имя';
+    name.nextElementSibling.className = 'error is-active-error';
     return false;
   }
   return true;
 }
 
-function validateTextarea(elem) {
-  if (elem.value === "") {
+/** Validate textarea, should not be empty
+   @param {string} text - value of textarea
+   @return {bool}
+*/
+function validateTextarea(text) {
+  if (elem.value === '') {
     elem.nextElementSibling.innerHTML =
-      "Сообщение не может быть пустым. Пожалуйста введите текст сообщения";
-    elem.nextElementSibling.className = "error is-active-error";
+      'Сообщение не может быть пустым. Пожалуйста введите текст сообщения';
+    elem.nextElementSibling.className = 'error is-active-error';
     return false;
   }
   return true;
 }
-
+ 
+/** Validate email input, should be valid email address
+  @param {string} email  - value of email input
+  @return {bool}    
+*/
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.value === "" || !re.test(email.value)) {
-    email.nextElementSibling.innerHTML = "Вы ввели неправильный email-адрес";
-    email.nextElementSibling.className = "error is-active-error";
+  if (email.value === '' || !re.test(email.value)) {
+    email.nextElementSibling.innerHTML = 'Вы ввели неправильный email-адрес';
+    email.nextElementSibling.className = 'error is-active-error';
     return false;
   }
   return true;
 }
 
-var form = document.getElementById("contact-form");
+var form = document.getElementById('contact-form');
 
-form.addEventListener("submit", function(event) {
-  var nameVal = document.getElementById("name"),
-    textVal = document.getElementById("message"),
-    email = document.getElementById("email");
+form.addEventListener('submit', function(event) {
+  var nameVal = document.getElementById('name');
+  var textVal = document.getElementById('message');
+  var email = document.getElementById('email');
   if (
     !validateEmail(email) ||
     !validateTextarea(textVal) ||
@@ -185,13 +201,13 @@ form.addEventListener("submit", function(event) {
 var xDown = null;
 var yDown = null;
 
-$(".doodle-item__container").on("touchstart", function(e) {
+$('.doodle-item__container').on('touchstart', function(e) {
   xDown = e.originalEvent.touches[0].clientX;
 });
 
-$(".doodle-item__container").on("touchmove", function(e) {
-  var self = $(this),
-    ribbon = self.parent().find(".doodle-item__ribbon").find("a").children();
+$('.doodle-item__container').on('touchmove', function(e) {
+  var self = $(this);
+  var ribbon = self.parent().find('.doodle-item__ribbon').find('a').children();
   if (!xDown) {
     return;
   }
@@ -199,23 +215,23 @@ $(".doodle-item__container").on("touchmove", function(e) {
   var xUp = e.originalEvent.touches[0].clientX, xDiff = xDown - xUp;
 
   if (xDiff > 100) {
-    self.addClass("clicked");
-    self.next().addClass("clicked-two");
-    ribbon.addClass("rotate");
-    var el = self.next().addClass("clicked-two").find(".preview"),
-      elLen = el.length;
+    self.addClass('clicked');
+    self.next().addClass('clicked-two');
+    ribbon.addClass('rotate');
+    var el = self.next().addClass('clicked-two').find('.preview');
+    var elLen = el.length;
     setTimeout(function() {
-      //el.each(function(item){
-      for (var k = 0; k < elLen; k++) {
-        if ($(el[k]).css("display") === "none") {
-          $(el[k]).delay(k * 150).fadeIn(200);
+      el.each(function(item) {
+        for (var k = 0; k < elLen; k++) {
+          if ($(el[k]).css('display') === 'none') {
+            $(el[k]).delay(k * 150).fadeIn(200);
+          }
         }
-      }
-      //});
+      });
     }, 800);
   } else if (xDiff < -50) {
-    self.prev().removeClass("clicked");
-    self.removeClass("clicked-two");
-    ribbon.removeClass("rotate");
+    self.prev().removeClass('clicked');
+    self.removeClass('clicked-two');
+    ribbon.removeClass('rotate');
   }
 });
